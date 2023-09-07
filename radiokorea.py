63cec3037d06f9ee28b45887769424a50a9628b7
Jay Kim <kimjingo@gmail.com>
2023-09-06 19:57:07 -0700

    remove testing line

--- radiokorea.py
+++ radiokorea.py
@@ -152,14 +152,14 @@ path = "bulletin/bbs/board.php?"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"}
 def main():
     keywords = {
-        # "웹",
-        # "개발",
-        # "프로그",
-        # "web",
-        # "python",
-        # "django",
-        # "php",
-        # "vue",
+        "웹",
+        "개발",
+        "프로그",
+        "web",
+        "python",
+        "django",
+        "php",
+        "vue",
         "develop",
     }
     args = "bo_table=c_jobs&sca=&sfl=wr_subject&stx="
